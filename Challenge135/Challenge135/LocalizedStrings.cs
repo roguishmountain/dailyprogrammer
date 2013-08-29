0321187e47e4231b580ea2639499cffbd13ba7a8
Samantha Schwarz <roguishmountain@gmail.com>
2013-08-29 13:09:14 -0400

    initial commit
    daily programmer reddit challenge #135

---
+++ Challenge135/Challenge135/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Challenge135.Resources;
+
+namespace Challenge135
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
