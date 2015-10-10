dc0e624c585c84684b1eae71b6dccf5a28e78f78
roguishmountain <roguishmountain@gmail.com>
2015-10-10 16:27:07 -0400

    no bonus yet

--- miniChallenges24Palin.py
+++ miniChallenges24Palin.py
@@ -10,3 +10,16 @@ Given: input: the minimum number of digits to consider
 Output: A list of palindromic numbers. You can decide how (and if) to end the sequence.
 '''
 
+n = sys.argv[1]
+
+def palindrome(n):
+    count = 0
+    for num in range(0, n):
+
+        splitNum = list(str(num))
+        splitNum = splitNum[::-1]
+        #print "".join(splitNum)
+        if "".join(splitNum) == str(num):
+            print num
+
+palindrome(int(n))
